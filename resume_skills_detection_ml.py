43b2aca7decb71d5c793226a0c334398bae9fe14
CIRIL SHAJU <75011401+ciril9955@users.noreply.github.com>
2023-09-22 22:02:20 +0530

    Update resume_skills_detection_ml.py

--- resume_skills_detection_ml.py
+++ resume_skills_detection_ml.py
@@ -12,11 +12,8 @@ logger = logging.getLogger(__name__)
 # Load the spaCy model
 nlp = spacy.load("en_core_web_sm")
 
-# Define predefined skills
-predefined_skills = ["Python", "Java", "Machine Learning", "Data Analysis", "Power BI", "Sales", "Marketing"]
-
 # Function to extract skills from text using regular expressions
-def extract_skills(text):
+def extract_skills(text, predefined_skills):
     extracted_skills = []
     skill_pattern = r'\b(?:' + '|'.join(re.escape(skill) for skill in predefined_skills) + r')\b'
     matches = re.findall(skill_pattern, text, flags=re.IGNORECASE)
@@ -25,55 +22,59 @@ def extract_skills(text):
     return list(set(extracted_skills))
 
 # Function to parse PDF file
-def parse_pdf(pdf_path):
+def parse_pdf(pdf_path, predefined_skills):
     skills = []
     try:
         pdf_reader = PdfReader(pdf_path)
         for page in pdf_reader.pages:
             text = page.extract_text()
-            skills += extract_skills(text)
+            skills += extract_skills(text, predefined_skills)
     except Exception as e:
         logger.error(f"Error parsing PDF '{pdf_path}': {str(e)}")
     return skills
 
 # Function to parse Word document
-def parse_word_doc(docx_path):
+def parse_word_doc(docx_path, predefined_skills):
     skills = []
     try:
         doc = Document(docx_path)
         for paragraph in doc.paragraphs:
             text = paragraph.text
-            skills += extract_skills(text)
+            skills += extract_skills(text, predefined_skills)
     except Exception as e:
         logger.error(f"Error parsing Word document '{docx_path}': {str(e)}")
     return skills
 
 # Function to parse a single resume
-def parse_resume(resume_path):
+def parse_resume(resume_path, predefined_skills):
     _, file_extension = os.path.splitext(resume_path)
     if file_extension.lower() == ".pdf":
-        skills = parse_pdf(resume_path)
+        skills = parse_pdf(resume_path, predefined_skills)
     elif file_extension.lower() == ".docx":
-        skills = parse_word_doc(resume_path)
+        skills = parse_word_doc(resume_path, predefined_skills)
     else:
         logger.error(f"Unsupported file format for '{resume_path}'")
         raise ValueError("Unsupported file format")
     return skills
 
 # Function to process all resumes in a folder
-def process_resumes_in_folder(folder_path):
+def process_resumes_in_folder(folder_path, predefined_skills):
     matching_skills_all_resumes = {}
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             if file.lower().endswith((".pdf", ".docx")):
                 resume_path = os.path.join(root, file)
-                matching_skills = parse_resume(resume_path)
+                matching_skills = parse_resume(resume_path, predefined_skills)
                 matching_skills_all_resumes[resume_path] = matching_skills
     return matching_skills_all_resumes
 
+# User input for predefined skills
+user_input_skills = input("Enter predefined skills (comma-separated): ")
+predefined_skills = [skill.strip() for skill in user_input_skills.split(',')]
+
 # Resume folder path
 folder_path = "C:/Users/ciril/OneDrive/Desktop/resume detection/resumes"
-matching_skills_all_resumes = process_resumes_in_folder(folder_path)
+matching_skills_all_resumes = process_resumes_in_folder(folder_path, predefined_skills)
 
 # Print matching skills for each resume along with the number of predefined and matching skills
 for resume_path, matching_skills in matching_skills_all_resumes.items():
